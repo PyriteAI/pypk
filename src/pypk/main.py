6700d60ec7fce1c7cea5d05799d7109d245faaf9
Miller Wilt <miller@pyriteai.com>
2021-06-18 11:14:07 -0400

    refactor: replace -v/--version flag with -p/--py-version

--- src/pypk/main.py
+++ src/pypk/main.py
@@ -24,8 +24,8 @@ def create(
     config: Path = typer.Option(None, "--config", "-c", help="Path to config file"),  # noqa: B008
     author: str = typer.Option(None, "--author", "-a", help="Author's name"),  # noqa: B008
     email: str = typer.Option(None, "--email", "-e", help="Author's email"),  # noqa: B008
-    version: str = typer.Option(  # noqa: B008
-        _CURRENT_VERSION, "--version", "-v", help="Minimum Python version supported"
+    python_version: str = typer.Option(  # noqa: B008
+        _CURRENT_VERSION, "--py-version", "-p", help="Minimum Python version supported"
     ),
     init_git: bool = typer.Option(True, "--init-git/--skip-git-init", help="Toggle Git initialization"),  # noqa: B008
 ) -> None:
@@ -39,7 +39,7 @@ def create(
 
     author = author if author else cfg.get("author")
     email = email if email else cfg.get("email")
-    version = version if version else cfg.get("version")
+    python_version = python_version if python_version else cfg.get("py_version")
 
     if author is None:
         exit_with_status("[Error] 'author' must be specified in either the config or via command line")
@@ -48,7 +48,7 @@ def create(
     if version is None:
         exit_with_status("[Error] 'version' must be specified in either the config or via command line")
 
-    core.create(package, author, email, version, init_git=init_git)
+    core.create(package, author, email, python_version, init_git=init_git)
 
 
 @app.command()
